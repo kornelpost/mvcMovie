0c79be92c0f0daab9b11c2e4af132370d5840b75
kornelpost <post.kornel@gmail.com>
2021-03-04 15:14:34 +0100

    added rating

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -26,6 +26,7 @@ namespace MvcMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
+                        Rating = "R",
                         Price = 7.99M
                     },
 
@@ -34,6 +35,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 8.99M
                     },
 
@@ -42,6 +44,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 9.99M
                     },
 
@@ -50,8 +53,18 @@ namespace MvcMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
+                        Rating = "R",
                         Price = 3.99M
-                    }
+                    },
+
+                     new Movie
+                     {
+                         Title = "Tabaluga",
+                         ReleaseDate = DateTime.Parse("1969-4-15"),
+                         Genre = "Cartoon",
+                         Rating = "S",
+                         Price = 33.99M
+                     }
                 );
                 context.SaveChanges();
             }
